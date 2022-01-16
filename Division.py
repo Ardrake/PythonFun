132a58a7ccf87b7ded1ce5b841ca2361bafb47f7
Andre <andrecooke@hotmail.com>
2022-01-15 19:34:54 -0500

    typo and comments

--- Division.py
+++ Division.py
@@ -3,7 +3,7 @@ while True:
     # Valid que l'entrez est un nombre entier
     try:
         numerateur =int(input("Entrez le numerateur: "))
-        break  #on sort de du while loop et va a l'étape suivante
+        break  #La conversion fonctionne!! on sort de du while loop avec le break et va a l'étape suivante
     except ValueError:
         print("Oups veuillez entrez un nombre entier")
 
@@ -13,8 +13,9 @@ while True:
     # valide que l'entrez est un nombre entier
     try:
         denominateur = int(input("Entrez le dénominateur: "))
+        # on valide que c'est pas zero avant de proceder
         if denominateur != 0:
-            break # On sort de la loop et on va a l'étape suivante
+            break #La conversion fonctionne!! on sort de du while loop avec le break et va a l'étape suivante
         else :
             # Si égale a zéro on print le message d'erreur
             print("Erreur division par zéro!! veuillez entrez un autre valeur ")
